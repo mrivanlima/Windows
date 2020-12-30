5ff50841f94f1f49e4de2c2758b872951dd202c1
Ivan Lima <ilima@gcr1.com>
2020-12-29 21:57:38 -0500

    Change ini

--- DatabaseHealth/PythonApplication2/ConnectionParser.py
+++ DatabaseHealth/PythonApplication2/ConnectionParser.py
@@ -6,9 +6,9 @@ from configparser import ConfigParser
 def config(filename):
     cf = ConfigParser()
     cf.read(filename)
-    _database = cf.get("SQLSERVER", "database")
-    _host = cf.get("SQLSERVER", 'host')
-    _user = cf.get("SQLSERVER", 'user')
-    _pwd = cf.get("SQLSERVER", 'pwd')
+    _database = cf.get("SQLSERVERWINDOWS", "database")
+    _host = cf.get("SQLSERVERWINDOWS", 'host')
+    _user = cf.get("SQLSERVERWINDOWS", 'user')
+    _pwd = cf.get("SQLSERVERWINDOWS", 'pwd')
     return _database, _host, _user, _pwd
   
