f758fd2cb4b2f5c58fcbef257f8cc8e27011869b
Ivan Lima <ilima@gcr1.com>
2020-12-31 12:33:47 -0500

    AWS Python

--- AWSManager/AWSManager/AWSManager.py
+++ AWSManager/AWSManager/AWSManager.py
@@ -1,1 +1,12 @@
 
+import logging
+import boto3
+
+
+bucket_name = 'mrivanlima1'
+LocationConstraint = 'us-west-2'
+
+
+s3_client = boto3.client('s3')
+location = {'LocationConstraint' : LocationConstraint}
+s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
